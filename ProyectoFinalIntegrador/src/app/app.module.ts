7ceda35e0b1c6403872da18786d05c4f45cdc3eb
bnacevedo <braian_29_94@hotmail.com>
2022-08-31 10:37:22 -0300

    bootstrap

--- ProyectoFinalIntegrador/src/app/app.module.ts
+++ ProyectoFinalIntegrador/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { RedsocialComponent } from './components/redsocial/redsocial.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    RedsocialComponent
+    RedsocialComponent,
+    BannerComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule,
