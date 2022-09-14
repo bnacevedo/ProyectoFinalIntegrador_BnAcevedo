84ba6997e3b8ceabd9c7335ac3ada903a7e24400
bnacevedo <braian_29_94@hotmail.com>
2022-09-14 08:47:53 -0300

    homelogin

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { HardskillsComponent } from './components/hardskills/hardskills.componen
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import {HttpClient, HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import {HttpClient, HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HardskillsComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
